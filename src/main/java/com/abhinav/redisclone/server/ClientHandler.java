387541eec69edddfb6ef60230be67c9e5c742877
Abhinav Raj <242582505+abhinavraj-git@users.noreply.github.com>
2026-08-05 00:21:12 +0530

    Implemented Redis transactions (MULTI, EXEC, DISCARD)

--- src/main/java/com/abhinav/redisclone/server/ClientHandler.java
+++ src/main/java/com/abhinav/redisclone/server/ClientHandler.java
@@ -3,9 +3,13 @@ import com.abhinav.redisclone.commands.CommandDispatcher;
 import com.abhinav.redisclone.protocol.RespParser;
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientHandler implements Runnable {
     private final Socket clientSocket;
+    private boolean inTransaction = false;
+    private final List<String[]> queuedCommands = new ArrayList<>();
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
     }
@@ -28,7 +32,50 @@ public class ClientHandler implements Runnable {
                 for (String argument : arguments) {
                     System.out.println(argument);
                 }
-                commandDispatcher.dispatch(arguments, outputStream);
+
+                String command = arguments[0].toUpperCase();
+                if (command.equals("MULTI")) {
+                    if (inTransaction) {
+                        outputStream.write("-ERR MULTI calls can not be nested\r\n".getBytes());
+                        outputStream.flush();
+                        continue;
+                    }
+                    inTransaction = true;
+                    queuedCommands.clear();
+
+                    outputStream.write("+OK\r\n".getBytes());
+                    outputStream.flush();
+
+                } else if (command.equals("EXEC")) {
+                    if (!inTransaction) {
+                        outputStream.write("-ERR EXEC without MULTI\r\n".getBytes());
+                        outputStream.flush();
+                        continue;
+                    }
+                    inTransaction = false;
+                    for (String[] queuedCommand : queuedCommands) {
+                        commandDispatcher.dispatch(queuedCommand, outputStream);
+                    }
+                    queuedCommands.clear();
+
+                }else if (command.equals("DISCARD")) {
+                    if (!inTransaction) {
+                        outputStream.write("-ERR DISCARD without MULTI\r\n".getBytes());
+                        outputStream.flush();
+                        continue;
+                    }
+                    queuedCommands.clear();
+                    inTransaction = false;
+                    outputStream.write("+OK\r\n".getBytes());
+                    outputStream.flush();
+
+                }else if (inTransaction) {
+                    queuedCommands.add(arguments);
+                    outputStream.write("+QUEUED\r\n".getBytes());
+                    outputStream.flush();
+                } else {
+                    commandDispatcher.dispatch(arguments, outputStream);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
